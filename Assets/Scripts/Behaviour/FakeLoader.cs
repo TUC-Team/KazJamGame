4fe648e51710a1cf34ce19fc218e9160d6910570
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-01-28 20:00:53 +0200

    ui loadscene

--- Assets/Scripts/Behaviour/FakeLoader.cs
+++ Assets/Scripts/Behaviour/FakeLoader.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class FakeLoader : MonoBehaviour {
     void Update() {
-	    if ( Input.anyKeyDown ) {
-		    SceneManager.LoadScene(1);
-	    }
+	   // if ( Input.anyKeyDown ) {
+		   // SceneManager.LoadScene(1);
+	   // }
     }
 }
