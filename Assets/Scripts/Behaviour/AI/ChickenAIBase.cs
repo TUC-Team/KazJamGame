b73a9e00dfcfcbc2c6264f0959a8289700c64699
Sergey <sgorey@bk.ru>
2021-01-28 18:33:04 +0800

    Начал делать руку

---
+++ Assets/Scripts/Behaviour/AI/ChickenAIBase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class ChickenAIBase : MonoBehaviour
+{
+    public bool IsKilled { get; private set; } = false;
+    
+    [SerializeField] private float stayingTime = 2f;
+    [SerializeField] private float targetStoppingDistance = .5f;
+    
+    private NavMeshAgent agent;
+    private Vector3 targetPosition;
+    private float timeInTargetPosition;
+    private HandAIBase hand;
+    private Collider collider;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        collider = GetComponent<Collider>();
+        hand = FindObjectOfType<HandAIBase>();
+        
+        hand.RegisterChicken(this);
+
+        timeInTargetPosition = 0;
+    }
+
+    private void Start()
+    {
+        targetPosition = transform.position;
+        ChangeTargetRandom();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(targetPosition);
+
+        if (Mathf.Abs(Vector3.Distance(transform.position, targetPosition)) <= targetStoppingDistance)
+        {
+            timeInTargetPosition += Time.deltaTime;
+        }
+
+        if (timeInTargetPosition >= stayingTime)
+        {
+            ChangeTargetRandom();
+        }
+    }
+
+    public void Kill()
+    {
+        IsKilled = true;
+        
+        hand.RemoveChicken(this);
+        
+        // play anim ...
+        print("chicken " + gameObject.name + " was killed");
+
+        transform.localScale = new Vector3(1, .05f, 1);
+
+        agent.enabled = false;
+        collider.enabled = false;
+        this.enabled = false;
+    }
+
+    private void ChangeTargetRandom()
+    {
+        var position = transform.position;
+        Vector3 target = position;
+
+        float x = Random.Range(-4f, 4f);
+        float z = Random.Range(-4f, 4f);
+
+        target.x += x;
+        target.y = position.y;
+        target.z += z;
+
+        timeInTargetPosition = 0;
+        
+        targetPosition = target;
+    }
+}
---
+++ Assets/Scripts/Behaviour/AI/HandAIBase.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HandAIBase : MonoBehaviour
+{
+    [SerializeField] private HandState startState;
+    [SerializeField] private float attackRate = 2f;
+    
+    private NavMeshAgent agent;
+    private ChickenAIBase targetChicken;
+    private ChickenAIBase chickenInAttackRange;
+    private Animator animator;
+    private List<ChickenAIBase> chickens = new List<ChickenAIBase>();
+    private HandState state;
+    private float timeSinceLastAttack = 0;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private IEnumerator Start()
+    {
+        targetChicken = FindNearestChicken();
+        animator.SetTrigger("appear");
+
+        state = startState;
+
+        // Ждем пока рука не спустится и переводим ее в состояние охоты
+        yield return new WaitForSeconds(2.5f);
+
+        state = HandState.Hunt;
+    }
+
+    private void Update()
+    {
+        timeSinceLastAttack += Time.deltaTime;
+            
+        switch (state)
+        {
+            case HandState.Hunt:
+                Hunt();
+                break;
+            case HandState.Patrol:
+                Patrol();
+                break;
+        }
+    }
+
+    public void RegisterChicken(ChickenAIBase chicken)
+    {
+        chickens.Add(chicken);
+    }
+
+    public void RemoveChicken(ChickenAIBase chicken)
+    {
+        chickens.Remove(chicken);
+
+        if (chicken == chickenInAttackRange)
+            chickenInAttackRange = null;
+    }
+
+    private ChickenAIBase FindNearestChicken()
+    {
+        float minDist = Mathf.Infinity;
+        ChickenAIBase nearestChicken = null;
+        
+        foreach (var chicken in chickens)
+        {
+            if (chicken.IsKilled)
+                continue;
+            
+            float dist = Vector3.Distance(transform.position, chicken.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearestChicken = chicken;
+            }
+        }
+
+        return nearestChicken;
+    }
+
+    private void PlayAttackAnim()
+    {
+        animator.SetTrigger("attack");
+    }
+
+    private void Hunt()
+    {
+        targetChicken = FindNearestChicken();
+
+        if (targetChicken == null)
+        {
+            print("Все курицы закончились");
+            return;
+        }
+        
+        agent.SetDestination(targetChicken.transform.position);
+
+        if (chickenInAttackRange != null && timeSinceLastAttack >= attackRate)
+        {
+            PlayAttackAnim();
+            timeSinceLastAttack = 0;
+        }
+    }
+    
+    private void Patrol(){}
+
+    private void OnTriggerStay(Collider other)
+    {
+        var chicken = other.GetComponent<ChickenAIBase>();
+
+        if (chicken == null)
+            return;
+
+        if (chicken.IsKilled)
+            return;
+
+        chickenInAttackRange = chicken;
+    }
+
+    private void OnAttackAnimation()
+    {
+        chickenInAttackRange.Kill();
+        chickenInAttackRange = null;
+    }
+}
+
+enum HandState
+{
+    Hunt, Patrol, Appearing
+}
