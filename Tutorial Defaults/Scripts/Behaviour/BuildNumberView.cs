56f1a4298defee5c075c688f6b93e16d562c899b
KonH <konh@yandex.ru>
2021-01-29 21:58:19 +0700

    Remove unwanted test project

--- Tutorial Defaults/Scripts/Behaviour/BuildNumberView.cs
+++
@@ -1,9 +0,0 @@
-﻿using TMPro;
-using UnityEngine;
-
-[RequireComponent(typeof(TMP_Text))]
-public class BuildNumberView : MonoBehaviour {
-	void Start() {
-		GetComponent<TMP_Text>().text = Application.version;
-	}
-}
