e1e6e146eee3c7a2c2c877c9ede026c5c2f4a202
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-01-29 20:15:35 +0200

    script sensetive mouse

--- Assets/Scripts/UI/Mouse_Control.cs
+++ Assets/Scripts/UI/Mouse_Control.cs
@@ -3,7 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Mouse_Control : MonoBehaviour
 {
-    public Slider mouse;
+    public Slider mouse_X;
+    public Slider mouse_Y;
+
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+
+
 }
+
+
+
