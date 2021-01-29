8f6ab78842fba2f972afcdee4222ec2036fb472a
KonH <konh@yandex.ru>
2021-01-29 23:53:12 +0700

    Add finish handler with scene transition

---
+++ Assets/Scripts/Behaviour/FinishHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishHandler : MonoBehaviour {
+	public void OnFinish(bool success) {
+		var sceneName = success ? "WinScene" : "LoseScene";
+		SceneManager.LoadScene(sceneName);
+	}
+}
