8d419a01c11ee5035b593add4ef4a376e33bd298
KonH <konh@yandex.ru>
2021-01-30 11:20:40 +0700

    Remove unwanted change

--- Assets/Scripts/Behaviour/BuildNumberView.cs
+++ Assets/Scripts/Behaviour/BuildNumberView.cs
@@ -1,12 +1,9 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(TMP_Text))]
 public class BuildNumberView : MonoBehaviour {
 	void Start() {
 		GetComponent<TMP_Text>().text = Application.version;
 	}
-
-
 }
