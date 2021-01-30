9680236f6aca0afd12f9f0eec334d3566a32543f
sxidfx <dernovoyvitaly@gmail.com>
2021-01-30 16:28:21 +0200

    add weapon, chicken, ui

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,38 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID TEST_LOOP_NEW_UPD = 742047857U;
+        static const AkUniqueID ALL_CHIKEN = 1878518477U;
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID BACK = 1559875400U;
+        static const AkUniqueID CHIC_DEATH = 3244530683U;
+        static const AkUniqueID CLICK = 1584507803U;
+        static const AkUniqueID GUN = 915214409U;
+        static const AkUniqueID GUN_STOP = 3664266748U;
+        static const AkUniqueID MECH_IDLE = 3137477943U;
+        static const AkUniqueID MECH_WEAPON = 3858988253U;
+        static const AkUniqueID MESH_FOOT = 2335306597U;
+        static const AkUniqueID OK = 1651971899U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MECH
+        {
+            static const AkUniqueID GROUP = 2657888854U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID EARTH = 3747705955U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace MECH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CHIKEN_SUM = 2488984241U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -24,7 +54,12 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID CHIKEN = 1718040157U;
+        static const AkUniqueID ENEMY = 2299321487U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PC = 1635194334U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
