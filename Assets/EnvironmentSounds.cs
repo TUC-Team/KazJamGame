22d5be03cb0c6bec36ee0a7af9441d8911544bc8
sxidfx <dernovoyvitaly@gmail.com>
2021-01-30 21:19:21 +0200

    add Game Object Sound, ambient, weapon

---
+++ Assets/EnvironmentSounds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSounds :
+    MonoBehaviour
+{
+    public string soundParameter;
+    public AK.Wwise.Event chic_sum;
+
+    private ChickenManager chickenManager;
+
+    private int InitialChickenCount = 0;
+
+    protected void Start()
+    {
+        chickenManager = FindObjectOfType<ChickenManager>();
+        InitialChickenCount = chickenManager.ChickensCount;
+        chic_sum.Post(gameObject);
+    }
+
+    protected void Update()
+    {
+        var normalizedCount = (int)Math.Round(chickenManager.ChickensCount * 100.0) / InitialChickenCount;
+        AkSoundEngine.SetRTPCValue(soundParameter, normalizedCount);
+    }
+}
---
+++ Assets/WwiseAudioSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseAudioSystem : MonoBehaviour
+{
+    public AK.Wwise.Event weapon_start;
+    public AK.Wwise.Event weapon_stop;
+    private bool _shotFired;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void StartShootingSerie()
+    {
+        if (!_shotFired)
+        {
+            _shotFired = true;
+            weapon_start.Post(gameObject);
+        }
+    }
+
+    public void StopShootingSerie()
+    {
+        if (_shotFired)
+        {
+            // play sound
+            _shotFired = false;
+            weapon_stop.Post(gameObject);
+        }
+    }
+}
