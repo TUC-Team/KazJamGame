700fe9b233eea97fe62bb9c5a1f374208c271a7a
KonH <konh@yandex.ru>
2021-01-31 15:25:42 +0700

    Remove old pause implementation

--- Assets/Scripts/Behaviour/ChickenManager.cs
+++ Assets/Scripts/Behaviour/ChickenManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 public class ChickenManager : MonoBehaviour
 {
     bool active;
-    public Transform CurrentPlayerPosition;
     public List<ChickenAIBase> Chickens { get; private set; } = new List<ChickenAIBase>();
     public int ChickensCount => Chickens.Count;
 
@@ -32,49 +31,5 @@ public class ChickenManager : MonoBehaviour
 
         if (ChickensCount == 0)
             _gameController.EndGame(false);
-    }
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            active = !active;
-            Time.timeScale = (active) ? 1f : 0;
-            savePosition();
-
-            SceneManager.LoadScene("MenuScene");
-                }
-        else { loadPosition();
-            { }
-        }
-
-    }
-    public void savePosition()
-    {
-
-        Transform CurrentPlayerPosition = this.gameObject.transform;
-
-        PlayerPrefs.SetFloat("PosX", CurrentPlayerPosition.position.x); // т.к. автоматической работы
-        PlayerPrefs.SetFloat("PosY", CurrentPlayerPosition.position.y); // с массивами нет, разбиваем на
-        PlayerPrefs.SetFloat("PosZ", CurrentPlayerPosition.position.z);  // отдельные float и записываем
-
-        PlayerPrefs.SetFloat("AngX", CurrentPlayerPosition.eulerAngles.x);
-        PlayerPrefs.SetFloat("AngY", CurrentPlayerPosition.eulerAngles.y);
-
-        PlayerPrefs.SetString("level", Application.loadedLevelName); // ещё можно писать/читать строки
-        PlayerPrefs.SetInt("level_id", Application.loadedLevel); // и целые
-    }
-
-    public void loadPosition()
-    {
-
-        Transform CurrentPlayerPosition = this.gameObject.transform;
-
-        Vector3 PlayerPosition = new Vector3(PlayerPrefs.GetFloat("PosX"),
-                    PlayerPrefs.GetFloat("PosY"), PlayerPrefs.GetFloat("PosZ"));
-        Vector3 PlayerDirection = new Vector3(PlayerPrefs.GetFloat("AngX"), // генерируем новые вектора
-                    PlayerPrefs.GetFloat("AngY"), 0);  // на основе загруженных данных
-
-        CurrentPlayerPosition.position = PlayerPosition; // и применяем их
-        CurrentPlayerPosition.eulerAngles = PlayerDirection;
     }
 }
