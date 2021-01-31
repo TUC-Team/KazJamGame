f556ec05a1132c3a0636de919fc191bfa60623b0
Sergey <sgorey@bk.ru>
2021-01-31 18:03:10 +0800

    Выигрыш при убийстве только 2 рук
    - увеличил калайдер попадания по руке

---
+++ Assets/Scripts/Behaviour/HandManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    public List<HandAIBase> Hands { get; private set; } = new List<HandAIBase>();
+    public int HandsCount => Hands.Count;
+
+    private GameController _gameController;
+
+    private void Awake()
+    {
+        _gameController = FindObjectOfType<GameController>();
+    }
+
+    public void RegisterHand(HandAIBase hand)
+    {
+        if (Hands.Contains(hand))
+            return;
+        
+        Hands.Add(hand);
+    }
+
+    public void RemoveHand(HandAIBase hand)
+    {
+        Hands.Remove(hand);
+        if (HandsCount == 0)
+            _gameController.EndGame(true);
+    }
+}
